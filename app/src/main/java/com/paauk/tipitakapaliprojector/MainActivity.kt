ac5b39e35acee4340cf6f4915e8d2cb6c9a894ab
Bhante Subhuti <bksubhuti@gmail.com>
2020-11-08 21:59:18 +0630

    added various changes.. for portrait and for restore session

--- app/src/main/java/com/paauk/tipitakapaliprojector/MainActivity.kt
+++ app/src/main/java/com/paauk/tipitakapaliprojector/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.paauk.tipitakapaliprojector
 
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
 import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+     fun setorientation(){
+        //
+
+
+    }
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,12 +50,50 @@ class MainActivity : AppCompatActivity() {
             allowFileAccess = true
             allowFileAccessFromFileURLs = true
             allowUniversalAccessFromFileURLs = true
-            displayZoomControls = true
+            displayZoomControls = false
             builtInZoomControls = true
             useWideViewPort = true
             loadWithOverviewMode = true
         }
+        // here we can set the orientation according to settings..
 
-        webView.loadUrl("file:///android_asset/tipitaka_projector_data/index.htm");
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+//        webView.settings.javaScriptEnabled = true
+  //      webView.addJavascriptInterface(JSBridge(this), "JSBridge")
+        webView.loadUrl("file:///android_asset/tipitaka_projector_data/index.htm")
+
+
+
+
+    }
+
+   /* override fun onConfigurationChanged(newConfig: Configuration) {
+        // Checks the orientation of the screen
+            super.onConfigurationChanged(newConfig)
+
+    }
+*/
+
+
+
+
+}
+/*
+class JSBridge(){
+    private var mainActivity = null
+    constructor () {}
+    constructor(inst: ma){
+        mainActivity = ma 
+
+    }
+
+
+    fun showMessageInNative(message:String){
+        mainActivity.setorientation()
+
     }
 }
+
+
+ */
